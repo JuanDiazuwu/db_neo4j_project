a499a5d9eff96132ff6190c43ef50ef378d5a43d
Fuan200 <juandiazfdez1992@gmail.com>
2024-05-23 13:40:41 -0600

    adding CORS to connecto with fronted

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 #from server.routes.users import user
 from server.routes.employees import employee
@@ -9,6 +10,16 @@ app = FastAPI()
 @app.get('/')
 def hello_world():
     return 'Server is running!!!'
+
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #app.include_router(user)
 app.include_router(employee)
